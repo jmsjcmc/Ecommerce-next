92b02d6b9e08ffa3ef7f99ca77e72e37310e25f9
jmsjcmc <jecemecojamestabilog@gmail.com>
2025-10-26 08:47:50 +0800

    feat: add contant routes

--- components/Container.tsx
+++ components/Container.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function Container({children, className} : {children: React.ReactNode; className?: string;}) {
   return (
-    <div className={cn('max-w-7xl mx-auto px-4', className)}>
+    <div className={cn('max-w-screen-xl mx-auto px-4', className)}>
       {children}
     </div>
   )
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,16 +1,19 @@
-import React from 'react'
-import Container from './Container'
-import Logo from './Logo'
+import React from "react";
+import Container from "./Container";
+import Logo from "./Logo";
+import HeaderMenu from "./HeaderMenu";
 
 export default function Header() {
   return (
-    <header className='bg-white py-5'>
-        <Container>
-            {/* Logo */}
-<Logo/>
-            {/* Nav Button */}
-            {/* Nav Admin */}
-        </Container>
+    <header className="bg-white py-5">
+      <Container className="flex items-center justify-between">
+        {/* Logo */}
+        <Logo />
+        <HeaderMenu />
+        <div className="">Others</div>
+        {/* Nav Button */}
+        {/* Nav Admin */}
+      </Container>
     </header>
-  )
+  );
 }
