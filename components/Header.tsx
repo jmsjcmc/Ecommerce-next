97ffb667603b3860ff111c47d9d50c2f1ee0fc11
jmsjcmc <jecemecojamestabilog@gmail.com>
2025-10-26 11:20:35 +0800

    feat: add sidemenu

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,15 +2,28 @@ import React from "react";
 import Container from "./Container";
 import Logo from "./Logo";
 import HeaderMenu from "./HeaderMenu";
+import SearchBar from "./SearchBar";
+import CartIcon from "./CartIcon";
+import FavoriteButton from "./FavoriteButton";
+import SignIn from "./SignIn";
+import MobileMenu from "./MobileMenu";
 
 export default function Header() {
   return (
     <header className="bg-white py-5">
-      <Container className="flex items-center justify-between">
+      <Container className="flex items-center justify-between text-lightColor">
         {/* Logo */}
-        <Logo />
+        <div className="w-auto md:w-1/3 flex items-center gap-2.5 justify-start md:gap-0">
+            <MobileMenu/>
+            <Logo />            
+        </div>
         <HeaderMenu />
-        <div className="">Others</div>
+        <div className="w-auto md:w-1/3 flex items-center justify-end gap-5">
+            <SearchBar/>
+            <CartIcon/>
+            <FavoriteButton/>
+            <SignIn/>
+        </div>
         {/* Nav Button */}
         {/* Nav Admin */}
       </Container>
---
+++ components/SignIn.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function SignIn() {
+  return (
+    <button className='text-sm font-semibold hover:text-darkColor text-lightColor hover:cursor-pointer hoverEffect'>
+      Log In
+    </button>
+  )
+}
